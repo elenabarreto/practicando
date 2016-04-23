aa8d7eff23bd9cb5b2b2c15598247c28ab45df46
unknown <silviaelenabarreto@gmail.com>
2016-04-23 17:31:23 -0300

    modificando

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,5 @@
+
+print "holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 nacimiento = int (input ('Ingrese la fecha de nacimiento: '))
 edad = int (input('Ahora, ingrese la fecha en la que querria saber la edad que tendra: '))
 edfinal = edad-nacimiento
