10f99cdd61df6463c4bcb3857190b756c2006984
unknown <silviaelenabarreto@gmail.com>
2016-04-23 17:27:54 -0300

    subiendo archivos

---
+++ ejercicio1.py
@@ -0,0 +1,4 @@
+nacimiento = int (input ('Ingrese la fecha de nacimiento: '))
+edad = int (input('Ahora, ingrese la fecha en la que querria saber la edad que tendra: '))
+edfinal = edad-nacimiento
+print "La edad que tendra sera:",edfinal
